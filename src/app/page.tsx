e028a1a6b6a76adbc047f21562e90f7be6f6dfa4
A.Jesus.G <93790687+AJesusMG@users.noreply.github.com>
2024-08-14 20:03:17 -0600

    New landing (#59)
    * Feat: Primera Seccion de la LandingPage
    
    * Feat: Añadi la seccion de Plataforma
    
    * Feat: Añadi casi toda la Landing Page Nueva

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 'use client'
 
+import { Footer7 } from "@/Components/Footer7/Footer7";
 import { Header26 } from "@/Components/Header26/Header26";
+import { Layout121 } from "@/Components/Layout121/Layout121";
 import { Layout16 } from "@/Components/Layout16/Layout16";
 import { Navbar1 } from "@/Components/Navbar1/Navbar1";
+import { Pricing18 } from "@/Components/Pricing18/Pricing18";
+import { Testimonial3 } from "@/Components/Testimonial3/Testimonial3";
 import React from "react";
 
 export default function LandingPage() {
@@ -11,6 +15,10 @@ export default function LandingPage() {
       <Navbar1 />
       <Header26 />
       <Layout16/>
+      <Layout121/>
+      <Pricing18/>
+      <Testimonial3/>
+      <Footer7/>
     </>
   );
 }
