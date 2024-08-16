caad026070d786c3fbbd2f8435f1d4b54057621c
Emiliano Aquetzalli Obregon Reyes <eobregon05@alumnos.uaq.mx>
2024-08-16 11:03:25 -0600

    fix error repository 5.0

--- src/app/api/user/[email]/route.ts
+++ src/app/api/user/[email]/route.ts
@@ -1,75 +1,75 @@
-import { NextResponse, NextRequest } from "next/server";
-import  db from "@/libs/db";
+// import { NextResponse, NextRequest } from "next/server";
+// import  db from "@/libs/db";
 
-export async function PATCH(request : NextRequest, { params } : { params : { email : string } }) {
-    const email = params.email;
-    interface ReqBody { name : string, email : string, phoneNumber : string, instagramUrl : string, facebookURL : string }
+// export async function PATCH(request : NextRequest, { params } : { params : { email : string } }) {
+//     const email = params.email;
+//     interface ReqBody { name : string, email : string, phoneNumber : string, instagramUrl : string, facebookURL : string }
 
-    const data = await request.json() as ReqBody;
-    try{
+//     const data = await request.json() as ReqBody;
+//     try{
 
-    if(!email) {
-        return NextResponse.json({
-            message: "Email is required",
-        }, {
-            status: 400,
-        });
-    }
+//     if(!email) {
+//         return NextResponse.json({
+//             message: "Email is required",
+//         }, {
+//             status: 400,
+//         });
+//     }
 
-        const user = await db.user.findFirst({
-            where: {
-                email: String(email),
-            },
-        });
+//         const user = await db.user.findFirst({
+//             where: {
+//                 email: String(email),
+//             },
+//         });
 
-        if(user){
-            const userUpdated = await db.user.update(
-                {
-                    where: {
-                        email: String(email),
-                    },
-                    data: {
-                        name: data.name,
-                        phoneNumber: data.phoneNumber,
-                        instagramUrl: data.instagramUrl,
-                        facebookUrl: data.facebookURL, 
-                    },
-                }
-            )
-        }
+//         if(user){
+//             const userUpdated = await db.user.update(
+//                 {
+//                     where: {
+//                         email: String(email),
+//                     },
+//                     data: {
+//                         name: data.name,
+//                         phoneNumber: data.phoneNumber,
+//                         instagramUrl: data.instagramUrl,
+//                         facebookUrl: data.facebookURL, 
+//                     },
+//                 }
+//             )
+//         }
 
-    }catch(error) {
-        return NextResponse.json({
-            message: "User not found",
-        }, {
-            status: 404,
-        });
-    } 
-}
+//     }catch(error) {
+//         return NextResponse.json({
+//             message: "User not found",
+//         }, {
+//             status: 404,
+//         });
+//     } 
+// }
 
-export async function GET(request : NextRequest, { params } : { params : { email : string } }) {
-    const email = params.email;
-    try {
-        const user = await db.user.findFirst({
-            where: {
-                email: String(email),
-            },
-        });
+// export async function GET(request : NextRequest, { params } : { params : { email : string } }) {
+//     const email = params.email;
+//     try {
+//         const user = await db.user.findFirst({
+//             where: {
+//                 email: String(email),
+//             },
+//         });
 
-        if(!user){
-            return NextResponse.json({
-                message: "User not found",
-            }, {
-                status: 404,
-            });
-        }
+//         if(!user){
+//             return NextResponse.json({
+//                 message: "User not found",
+//             }, {
+//                 status: 404,
+//             });
+//         }
 
-        return NextResponse.json(user);
-    } catch (error) {
-        return NextResponse.json({
-            message: "User not found",
-        }, {
-            status: 404,
-        });
-    }
-}
+//         return NextResponse.json(user);
+//     } catch (error) {
+//         return NextResponse.json({
+//             message: "User not found",
+//         }, {
+//             status: 404,
+//         });
+//     }
+// }
