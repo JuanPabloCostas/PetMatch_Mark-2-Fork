a6f174aa473c28938af3ab9d31d486a275157fa2
Juan Pablo Costas Rueda <102332980+JuanPabloCostas@users.noreply.github.com>
2024-08-22 11:39:58 -0600

    Feat: Image commpression (#78)

--- src/app/api/uploadImage/route.ts
+++ src/app/api/uploadImage/route.ts
@@ -1,53 +1,58 @@
 import { ListObjectsCommand, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { NextRequest, NextResponse } from "next/server";
 import NameGenerator from "../../../utils/nameGenerator";
+import { formatImage, uploadImage } from "@/utils/fileHandlers";
 
 const Bucket = process.env.S3_BUCKET_NAME;
 const s3 = new S3Client({
-    credentials: {
-        accessKeyId: process.env.ACCESS_KEY_ID as string,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
-    },
-    region: process.env.S3_REGION as string, // Asegúrate de tener la región configurada.
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
+  },
+  region: process.env.S3_REGION as string, // Asegúrate de tener la región configurada.
 });
 
 export async function GET() {
-    try {
-        const response = await s3.send(new ListObjectsCommand({ Bucket }));
-        return NextResponse.json(response?.Contents ?? [], { status: 200 });
-    } catch (error) {
-        console.error("Error fetching objects from S3:", error);
-        return NextResponse.json({ code: 500, message: "Internal Server Error" }, { status: 500 });
-    }
+  try {
+    const response = await s3.send(new ListObjectsCommand({ Bucket }));
+    return NextResponse.json(response?.Contents ?? [], { status: 200 });
+  } catch (error) {
+    console.error("Error fetching objects from S3:", error);
+    return NextResponse.json({ code: 500, message: "Internal Server Error" }, { status: 500 });
+  }
 }
 
 export async function POST(request: NextRequest) {
-    try {
-        const formData = await request.formData();
-        const file = formData.get("image") as File;
+  try {
+    const formData = await request.formData();
+    const file = formData.get("image") as File;
 
-        if (!file || file.size === 0) {
-            return NextResponse.json({ code: 400, message: "No file provided" }, { status: 400 });
-        }
-
-        const Key = NameGenerator();
-        const Body = (await file.arrayBuffer()) as Buffer;
-
-        const uploadResponse = await s3.send(
-            new PutObjectCommand({
-                Bucket,
-                Key,
-                Body,
-                ACL: "public-read",
-                ContentType: file.type,
-            })
-        );
-
-        console.log("Upload response:", uploadResponse);
-
-        return NextResponse.json({ url: `https://${Bucket}.s3.amazonaws.com/${Key}` }, { status: 200 });
-    } catch (error) {
-        console.error("Error uploading file to S3:", error);
-        return NextResponse.json({ code: 500, message: "Failed to upload image" }, { status: 500 });
+    if (!file || file.size === 0) {
+      return NextResponse.json({ code: 400, message: "No file provided" }, { status: 400 });
     }
+
+    console.log("File:", file);
+    const Body = (await file.arrayBuffer()) as Buffer;
+
+
+    const resizedImage = await formatImage(Body);
+
+
+    if (!resizedImage) {
+      return NextResponse.json({ code: 500, message: "Failed to resize image" }, { status: 500 });
+    }
+
+    const uploadedImage = await uploadImage(resizedImage);
+
+
+    if (!uploadedImage) {
+      return NextResponse.json({ code: 500, message: "Failed to upload image" }, { status: 500 });
+    }
+
+    return NextResponse.json({ url: uploadedImage }, { status: 200 });
+
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ code: 500, message: "Failed to upload image" }, { status: 500 });
+  }
 }
---
+++ src/utils/fileHandlers.ts
@@ -0,0 +1,45 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import sharp from "sharp";
+import NameGenerator from "./nameGenerator";
+
+export async function formatImage(image: Buffer) {
+  try {
+    return await sharp(image)
+      .rotate()
+      .resize({width: 640, height: 640})
+      .toFormat("webp", { mozjpeg: true })
+      .toBuffer();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const s3Client = new S3Client({
+  region: process.env.S3_REGION as string,
+  credentials: {
+    accessKeyId: process.env.ACCESS_KEY_ID as string,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY as string,
+  },
+});
+
+export async function uploadImage(image: Buffer) {
+  try {
+    const key = NameGenerator() + ".webp";
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: process.env.S3_BUCKET_NAME as string,
+        Key: key,
+        Body: image,
+        ACL: "public-read",
+        ContentType: "image/webp",
+      })
+    );
+
+    return `https://${process.env.S3_BUCKET_NAME}.s3.amazonaws.com/${key}`;
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// module.exports = { resizeImage, uploadImage };
