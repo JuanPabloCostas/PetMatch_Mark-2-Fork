bd200d98e5aff31a6afe42b0e99c966ebf422db4
BryanBonilla <93222564+BryanBonilla@users.noreply.github.com>
2024-08-24 17:17:33 -0600

    feat: community by ID fixed and implemented timeDifference (#98)
    * feat: timeDifference, username and fullname added to Community
    
    * feat: community by ID fixed and implemented timeDifference
    
    ---------
    
    Co-authored-by: Juan Pablo Costas Rueda <102332980+JuanPabloCostas@users.noreply.github.com>

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -6,7 +6,7 @@ const prisma = new PrismaClient();
 export async function POST(request: NextRequest) {
   try {
     // Obtiene los datos del cuerpo de la solicitud
-    const { text, communityId, userId, parentId, imgUrl } = await request.json();
+    const { text, communityId, userId, parentId, imgUrl, createdAt } = await request.json();
 
     // Verifica que el campo 'text' no esté vacío
     if (!text) {
@@ -23,7 +23,8 @@ export async function POST(request: NextRequest) {
         communityId: communityId || null, 
         userId: userId || null,          
         parentId: parentId || null,    
-        imgUrl: imgUrl || null
+        imgUrl: imgUrl || null,
+        createdAt: createdAt, // Usamos el createdAt enviado desde la solicitud
       },
     });
 
@@ -44,42 +45,89 @@ export async function POST(request: NextRequest) {
 }
 
 export async function GET(request: NextRequest) {
-  try {
-      // Realiza la consulta para obtener los comentarios principales (sin parentId)
-      const comments = await prisma.comment.findMany({
-          where: {
-              parentId: null,  // Filtra los comentarios que no tienen un parentId
-          },
-          select: {
-              id: true,
-              text: true,
-              imgUrl: true,
-              user: {
-                  select: {
-                      fullname: true,
-                      username: true,
-                      photoUrl: true,
-                  },
-              },
-          },
-          orderBy: {
-              createdAt: 'desc', // Ordena los comentarios por la fecha de creación en orden descendente
-          },
-      });
+  // fecha y hora actual en formato UTC
+  const currentDateUTC = new Date();
 
-      return NextResponse.json({
-          code: 200,
-          message: "Comentarios obtenidos exitosamente.",
-          data: comments,
-      });
+  // offset en minutos para la zona horaria de México
+  const offset = -6 * 60; // Offset para CDT 
+
+  // fecha y hora local en México sumando el offset
+  const currentDateLocal = new Date(currentDateUTC.getTime() + offset * 60000);
+  // fecha y hora local a ISO-8601 DateTime
+  const currentDateLocalISO = currentDateLocal.toISOString();
+
+  console.log(currentDateLocalISO);
+
+  try {
+    // Realiza la consulta para obtener los comentarios principales (sin parentId)
+    const comments = await prisma.comment.findMany({
+      where: {
+        parentId: null,  // Filtra los comentarios que no tienen un parentId
+      },
+      select: {
+        id: true,
+        text: true,
+        createdAt: true,
+        imgUrl: true,
+        user: {
+          select: {
+            fullname: true,
+            username: true,
+            photoUrl: true,
+          },
+        },
+      },
+      orderBy: {
+        createdAt: 'desc', 
+      },
+    });
+
+      // Calcula la diferencia de tiempo para cada comentario
+      const commentsWithTimeDiff = comments.map(comment => {
+      const createdAtDate = new Date(comment.createdAt);
+      const timeDiff = currentDateLocal.getTime() - createdAtDate.getTime();
+
+      const seconds = Math.floor(timeDiff / 1000);
+      const minutes = Math.floor(seconds / 60);
+      const hours = Math.floor(minutes / 60);
+      const days = Math.floor(hours / 24);
+      const months = Math.floor(days / 30); // Aproximación de meses
+      const years = Math.floor(days / 365); // Aproximación de años
+
+      let timeDifference: string;
+
+      if (years > 0) {
+        timeDifference = `${years} año${years > 1 ? 's' : ''}`;
+      } else if (months > 0) {
+        timeDifference = `${months} mes${months > 1 ? 'es' : ''}`;
+      } else if (days > 0) {
+        timeDifference = `${days} día${days > 1 ? 's' : ''}`;
+      } else if (hours > 0) {
+        timeDifference = `${hours} hora${hours > 1 ? 's' : ''}`;
+      } else if (minutes > 0) {
+        timeDifference = `${minutes} minuto${minutes > 1 ? 's' : ''}`;
+      } else {
+        timeDifference = `${seconds} segundo${seconds > 1 ? 's' : ''}`;
+      }
+
+      return {
+        ...comment,
+        timeDifference,
+      };
+    });
+
+    return NextResponse.json({
+      code: 200,
+      message: "Comentarios obtenidos exitosamente.",
+      data: commentsWithTimeDiff,
+    });
   } catch (error) {
-      console.error("Error al obtener los comentarios:", error);
-      return NextResponse.json({
-          code: 500,
-          message: "Ocurrió un error al obtener los comentarios.",
-      });
+    console.error("Error al obtener los comentarios:", error);
+    return NextResponse.json({
+      code: 500,
+      message: "Ocurrió un error al obtener los comentarios.",
+    });
   } finally {
-      await prisma.$disconnect();
+    await prisma.$disconnect();
   }
 }
-
--- src/libs/actions/comment.actions.ts
+++ src/libs/actions/comment.actions.ts
@@ -22,63 +22,80 @@ export async function fetchChildrenComments(id: string) {
 }
 
 export async function sendComment(
-    commentText: string,
-    image: File | null,
-    userId: string,
-    parentId?: string
-  ): Promise<boolean> {
-    try {
-      let imgUrl = "";
-  
-      if (image) {
-        const formData = new FormData();
-        formData.append("image", image);
-  
-        // Primera solicitud para subir la imagen al bucket de AWS
-        const uploadResponse = await fetch("/api/uploadImage/community", {
-          method: "POST",
-          body: formData,
-        });
-  
-        if (uploadResponse.ok) {
-          const { url } = await uploadResponse.json();
-          imgUrl = url;
-          console.log("Imagen subida correctamente. URL:", url);
-          alert("Imagen subida correctamente. URL:" + url);
-        } else {
-          console.error("Error al subir la imagen. Estado:", uploadResponse.status);
-          alert("Error al subir la imagen. Estado:" + uploadResponse);
-          return false;
-        }
-      }
-  
-      // Segunda solicitud para enviar el comentario
-      const postFormData = {
-        text: commentText,
-        imgUrl,
-        userId,
-      };
-  
-      const postResponse = await fetch(parentId ? `/api/comments/children?id=${parentId}` : "/api/comments", {
+  commentText: string,
+  image: File | null,
+  userId: string,
+  parentId?: string
+): Promise<boolean> {
+  const currentDateUTC = new Date();
+
+  // Offset en minutos para la zona horaria de México (restar 6 horas)
+  const offset = -6 * 60;
+
+  // Fecha y hora local en México restando el offset
+  const currentDateLocal = new Date(currentDateUTC.getTime() + offset * 60000);
+
+  // Convertir la fecha a formato ISO 8601
+  const formattedDate = currentDateLocal.toISOString();
+
+  try {
+    let imgUrl = "";
+
+    if (image) {
+      const formData = new FormData();
+      formData.append("image", image);
+
+      // Primera solicitud para subir la imagen al bucket de AWS
+      const uploadResponse = await fetch("/api/uploadImage/community", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(postFormData),
+        body: formData,
       });
-  
-      if (postResponse.ok) {
-        console.log("Comentario enviado correctamente.");
-        return true;
+
+      if (uploadResponse.ok) {
+        const { url } = await uploadResponse.json();
+        imgUrl = url;
+        alert("Imagen subida correctamente. URL:" + url);
       } else {
-        console.error("Error al enviar el comentario. Estado:", postResponse.status);
+        console.error("Error al subir la imagen. Estado:", uploadResponse.status);
+        alert("Error al subir la imagen. Estado:" + uploadResponse);
         return false;
       }
-    } catch (error) {
-      console.error("Error al enviar el comentario:", error);
-      return false;
     }
+
+    // Segunda solicitud para enviar el comentario
+    const postFormData = {
+      text: commentText,
+      imgUrl,
+      userId,
+      createdAt: formattedDate, // Utilizamos la fecha formateada
+    };
+
+    const postResponse = await fetch(parentId ? `/api/comments/children?id=${parentId}` : "/api/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postFormData),
+    });
+
+    if (postResponse.status === 200) {
+      return true;
+    } else {
+      const errorData = await postResponse.json();
+      console.error("Error al enviar el comentario. Estado:", postResponse.status, "Mensaje:", errorData.message);
+      return false; // Devuelve false en caso de error
+    }
+
+  } catch (error) {
+    console.error("Error al enviar el comentario:", error);
+    return false;
   }
+}
+
+
+
+
+
   
   
   
