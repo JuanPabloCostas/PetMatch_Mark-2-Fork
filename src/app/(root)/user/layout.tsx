f42fea4199de38dce7e9c1145fdff755d489465b
Costas Rueda Juan Pablo <jcostas29@alumnos.uaq.mx>
2024-08-25 00:57:03 -0600

    fix nav?

--- src/app/(root)/user/layout.tsx
+++ src/app/(root)/user/layout.tsx
@@ -6,7 +6,8 @@ import Sidebar from "@/Components/SideBar/SideBar";
 export default function LayoutUser({ children }: { children: ReactNode }) {
 
   return (
-    <div className="flex xl:flex-row flex-col-reverse h-screen justify-between">
+    // <div className="flex xl:flex-row flex-col-reverse h-screen justify-between">
+    <div className="flex xl:flex-row flex-col-reverse h-svh justify-between">
         <Sidebar/>
       <div className="p-4 w-full pb-20 lg:h-screen overflow-auto">
         {children}
