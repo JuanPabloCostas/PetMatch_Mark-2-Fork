e91363a162902f6dabbf4063f4daa3e57d9e0b4e
Costas Rueda Juan Pablo <jcostas29@alumnos.uaq.mx>
2024-08-25 02:35:38 -0600

    Patch: Made Navbar more responsive on mobile and fixed community posts headers

--- src/Components/SideBar/SideBar.tsx
+++ src/Components/SideBar/SideBar.tsx
@@ -26,7 +26,7 @@ export default function Sidebar() {
 
   return (
     <nav
-      className="xl:h-screen shadow-xl xl:w-fit w-full flex justify-between lg:flex-col p-4"
+      className="xl:h-screen shadow-xl xl:w-fit w-full flex justify-between lg:flex-col p-1 sm:p-4"
       style={{ boxShadow: "1px 0 6px rgba(0, 0, 0, 0.05)" }}
     >
       <div className="flex xl:h-full xl:flex-col xl:gap-4 w-full justify-between flex-row">
@@ -37,7 +37,7 @@ export default function Sidebar() {
           alt="Logo"
           className="hidden xl:block -mt-6"
         />
-        <div className="flex flex-row gap-10 w-full justify-evenly xl:h-full xl:flex-col xl:gap-8 xl:mt-4 lg:gap-6 lg:mt-2 xl:text-center xl:items-center">
+        <div className="flex flex-row sm:gap-10 w-full justify-evenly xl:h-full xl:flex-col xl:gap-8 xl:mt-4 lg:gap-6 lg:mt-2 xl:text-center xl:items-center">
           <Tooltip content="Recomendaciones" placement="right" size="sm">
             <Button
               variant="light"
--- src/app/(root)/user/MobilePrincipalPage/page.tsx
+++ src/app/(root)/user/MobilePrincipalPage/page.tsx
@@ -3,79 +3,87 @@
 import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import PostCardMobile, { PostCardPropsMobile } from "@/Components/PostCardMobile/PostCardMobile";
+import Image from "next/image";
 
 interface FullPostProps {
-    id: string;
-    urlImage: string;
-    avatar: string;
-    fullname: string;
-    username: string;
-    content: string;
-    race: string;
-    size: string;
-    age: string;
-    instagram: string;
-    whatsapp: string;
-    facebook: string;
+  id: string;
+  urlImage: string;
+  avatar: string;
+  fullname: string;
+  username: string;
+  content: string;
+  race: string;
+  size: string;
+  age: string;
+  instagram: string;
+  whatsapp: string;
+  facebook: string;
 }
 
 export default function MobilePrincipalPage() {
 
-    const [posts, setPosts] = useState<PostCardPropsMobile[]>([]);
-    const router = useRouter();
+  const [posts, setPosts] = useState<PostCardPropsMobile[]>([]);
+  const router = useRouter();
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch("/api/posts");
-                if (!response.ok) {
-                    throw new Error("Failed to fetch data");
-                }
-                const data = await response.json();
-                console.log(data.data);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/posts");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        console.log(data.data);
 
-                const formattedPosts = data.data.map((post: any, index: number) => ({
-                    id: post.id, // Asigna el id correcto aquí
-                    urlImage: post.urlImage,
-                    avatar: post.avatar,
-                    fullname: post.fullname,
-                    username: post.username,
-                    content: post.content,
-                    race: post.race,
-                    size: post.size,
-                    age: post.age,
-                    instagram: post.instagram,
-                    whatsapp: post.whatsapp,
-                    facebook: post.facebook
-                }));
+        const formattedPosts = data.data.map((post: any, index: number) => ({
+          id: post.id, // Asigna el id correcto aquí
+          urlImage: post.urlImage,
+          avatar: post.avatar,
+          fullname: post.fullname,
+          username: post.username,
+          content: post.content,
+          race: post.race,
+          size: post.size,
+          age: post.age,
+          instagram: post.instagram,
+          whatsapp: post.whatsapp,
+          facebook: post.facebook
+        }));
 
-                setPosts(formattedPosts);
-            } catch (error) {
-                console.error("Error fetching data:", error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    const handleCardClick = (id: string) => {
-        router.push(`/user/MobilePrincipalPage/${id}`);
+        setPosts(formattedPosts);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
-    return (
-        <div className="flex flex-col w-full h-full">
-            <header className="w-full">
-                <h1 className="text-center font-bold text-xl">Tus Recomendaciones</h1>
-            </header>
-            <div className="grid grid-cols-3 gap-4 mt-4">
-                {posts.map((post, index) => (
-                    <PostCardMobile
-                        key={index}
-                        id={post.id}
-                        urlImage={post.urlImage}
-                        onClick={() => handleCardClick(post.id)} // Maneja el click
-                    />
-                ))}
-            </div>
-        </div>
-    )
+
+    fetchData();
+  }, []);
+
+  const handleCardClick = (id: string) => {
+    router.push(`/user/MobilePrincipalPage/${id}`);
+  };
+  return (
+    <div className="flex flex-col w-full h-full">
+      <header className="w-full">
+        <Image
+          src="/ZORRO_SIN1.webp"
+          width={50}
+          height={50}
+          alt="Logo"
+          className="absolute xl:hidden -mt-6"
+        />
+        <h1 className="text-center font-bold text-xl">Tus Recomendaciones</h1>
+      </header>
+      <div className="grid grid-cols-3 gap-4 mt-4">
+        {posts.map((post, index) => (
+          <PostCardMobile
+            key={index}
+            id={post.id}
+            urlImage={post.urlImage}
+            onClick={() => handleCardClick(post.id)} // Maneja el click
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
